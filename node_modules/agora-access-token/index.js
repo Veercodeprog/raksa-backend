5fff082e6d0afe7cf79acce425a05d68001e1d2b
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-06-17 21:01:29 +0530

    Remove .env and node_modules

--- node_modules/agora-access-token/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  RtcTokenBuilder: require('./src/RtcTokenBuilder').RtcTokenBuilder,
-  RtcRole: require('./src/RtcTokenBuilder').Role,
-  RtmTokenBuilder: require('./src/RtmTokenBuilder').RtmTokenBuilder,
-  RtmRole: require('./src/RtmTokenBuilder').Role
-}
